04fe33dfd9b215cecca5ba88e37d42e77d30a27a
Bjørn Madsen <bm@aeons.dk>
2012-10-16 15:26:02 +0200

    Put all code for each problem in one function.

--- problem004.go
+++ problem004.go
@@ -5,6 +5,61 @@ func init() {
 }
 
 func Solve4() (solution int) {
+	var isPalindromeDigit func(n, d int) bool
+
+	isPalindrome := func(n int) bool {
+		// fmt.Println(n)
+		switch {
+		// 1 digits
+		case n < 10:
+			return true
+			// 2 digits
+		case n < 100:
+			if n/10 == n%10 {
+				return true
+			}
+			// 3 digits
+		case n < 1000:
+			if n/100 == n%10 {
+				return true
+			}
+			// 4 digits
+		case n < 10000:
+			if f, l := n/1000, n%10; f == l {
+				return isPalindromeDigit((n-1000*f)/10, 2)
+			}
+			// 5 digits
+		case n < 100000:
+			if f, l := n/10000, n%10; f == l {
+				return isPalindromeDigit((n-10000*f)/10, 3)
+			}
+			// 6 digits
+		default:
+			if f, l := n/100000, n%10; f == l {
+				return isPalindromeDigit((n-100000*f)/10, 4)
+			}
+		}
+		return false
+	}
+
+	isPalindromeDigit = func(n, d int) bool {
+		switch d {
+		case 2:
+			if n/10 == n%10 {
+				return true
+			}
+		case 3:
+			if n/100 == n%10 {
+				return true
+			}
+		case 4:
+			if f, l := n/1000, n%10; f == l {
+				return isPalindromeDigit((n-1000*f)/10, 2)
+			}
+		}
+		return false
+	}
+
 	for i := 0; i < 1000; i++ {
 		for j := 0; j < 1000; j++ {
 			if k := i * j; isPalindrome(k) && k > solution {
@@ -13,57 +68,4 @@ func Solve4() (solution int) {
 		}
 	}
 	return
-}
-
-func isPalindrome(n int) bool {
-	// fmt.Println(n)
-	switch {
-	// 1 digits
-	case n < 10:
-		return true
-		// 2 digits
-	case n < 100:
-		if n/10 == n%10 {
-			return true
-		}
-		// 3 digits
-	case n < 1000:
-		if n/100 == n%10 {
-			return true
-		}
-		// 4 digits
-	case n < 10000:
-		if f, l := n/1000, n%10; f == l {
-			return isPalindromeDigit((n-1000*f)/10, 2)
-		}
-		// 5 digits
-	case n < 100000:
-		if f, l := n/10000, n%10; f == l {
-			return isPalindromeDigit((n-10000*f)/10, 3)
-		}
-		// 6 digits
-	default:
-		if f, l := n/100000, n%10; f == l {
-			return isPalindromeDigit((n-100000*f)/10, 4)
-		}
-	}
-	return false
-}
-
-func isPalindromeDigit(n, d int) bool {
-	switch d {
-	case 2:
-		if n/10 == n%10 {
-			return true
-		}
-	case 3:
-		if n/100 == n%10 {
-			return true
-		}
-	case 4:
-		if f, l := n/1000, n%10; f == l {
-			return isPalindromeDigit((n-1000*f)/10, 2)
-		}
-	}
-	return false
 }
--- problem005.go
+++ problem005.go
@@ -1,27 +1,25 @@
 package main
 
-var (
-	divisors = []int{7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19}
-)
-
 func init() {
 	solvers[5] = Solve5
 }
 
 func Solve5() (solution int) {
+	divisors := []int{7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19}
+
+	isValid := func(n int) bool {
+		for _, d := range divisors {
+			if n%d != 0 {
+				return false
+			}
+		}
+		return true
+	}
+
 	for i := 2520; ; i = i + 3*7*20 {
 		if isValid(i) {
 			return i
 		}
 	}
 	return
-}
-
-func isValid(n int) bool {
-	for _, d := range divisors {
-		if n%d != 0 {
-			return false
-		}
-	}
-	return true
 }
